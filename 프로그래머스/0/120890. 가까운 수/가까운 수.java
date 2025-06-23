398ee4068aa2f7c1926525ef6201cab6fa72e502
김희성 <s2563@e-mirim.hs.kr>
2025-06-23 23:14:56 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int[] array, int n) {
-        int small = Integer.MAX_VALUE;
+        int big = Integer.MAX_VALUE;
         int line;
         int answer = 0;
         for(int i = 0; i<array.length; i++){
             line = Math.abs(n-array[i]);
-                if(line <small){
-                    small = line;
+                if(line < big){
+                    big = line;
                     answer = array[i];
-                } else if(line == small && array[i] < answer)
+                } else if(line == big && array[i] < answer)
                     answer = array[i];
             }
         return answer;
