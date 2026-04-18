55008e6c861127152c64fd7e75b58dfcad738f7f
김희성 <s2563@e-mirim.hs.kr>
2026-04-19 00:23:54 +0900

    [level 0] Title: A 강조하기, Time: 9.25 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0; i < myString.length(); i++){
+            char ch = myString.charAt(i);
+            if(ch == 'a' || ch == 'A'){
+                answer += 'A';
+            }else{
+                answer += Character.toLowerCase(ch);
+            }
+        }
+        return answer;
+    }
+}
