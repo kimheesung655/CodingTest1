bf9acb88b5716e2bcb495e53d0c4a8f6e4acb6e3
김희성 <s2563@e-mirim.hs.kr>
2026-05-22 23:59:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int factorial = 1;
+        while (factorial <= n) {
+            answer++;
+            factorial *= answer;
+        }
+        return answer - 1;
+    }
+}
