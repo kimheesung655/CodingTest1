1de049178ee6306656c2d08581e8acbecd2bd8fb
김희성 <s2563@e-mirim.hs.kr>
2026-06-03 00:26:07 +0900

    Update 문자 개수 세기.js

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -3,10 +3,8 @@ function solution(my_string) {
     for(let x of my_string){
         let code = x.charCodeAt(0);
         if (code >= 65 && code <= 90) {
-            // 대문자 (A=65 ~ Z=90) -> 0번 ~ 25번 인덱스에 매핑
             answer[code - 65]++;
         } else if (code >= 97 && code <= 122) {
-            // 소문자 (a=97 ~ z=122) -> 26번 ~ 51번 인덱스에 매핑
             answer[code - 97 + 26]++;
         }
     }
