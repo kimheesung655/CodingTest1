cdb5b349501cc8ac798e3e35c078697ee3c67b8f
김희성 <s2563@e-mirim.hs.kr>
2026-06-21 21:08:35 +0900

    [level 0] Title: 코드 처리하기, Time: 2.88 ms, Memory: 51.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,26 @@
+function solution(code) {
+    let answer = '';
+    let mode = 0;
+    for (let i = 0; i < code.length; i++) {
+        if (code[i] === "1") {
+            if (mode === 0) mode = 1;
+            else mode = 0;
+        } 
+        else {
+            if(mode === 0) {
+                if(i % 2 === 0){
+                    answer += code[i];
+                }
+            } else {
+                if(i % 2 === 1){
+                    answer += code[i];
+                }
+            }
+        }
+    }
+    if(answer.length === 0){
+        return "EMPTY";
+    }
+    
+    return answer;
+}
